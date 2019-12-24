9b32fd644b1e7598fba926732c763e7dd2394ff7
xiantang <zhujingdi1998@gmail.com>
2019-12-24 23:43:12 +0800

    aa

---
+++ scala/fpinscala/src/main/scala/selection6/RNG.scala
@@ -0,0 +1,33 @@
+package selection6
+
+trait RNG {
+  def nextInt:(Int,RNG)
+
+}
+
+
+case class SimpleRNG(see:Long)extends RNG{
+  override def nextInt: (Int, RNG) = {
+    val newSeed = (see * 0x5DEECE66DL + 0xBL)&0xFFFFFFFFFFFFL
+    val newRNG = SimpleRNG(newSeed)
+    val n = (newSeed >>> 16).toInt
+    (n,newRNG)
+  }
+  def nonNegativeInt(rng:RNG):(Int,RNG)={
+    val (n,rng) = nextInt
+    if(n>=0){
+      (n,rng)
+    }else {
+      nonNegativeInt(rng)
+    }
+  }
+
+}
+object RNG{
+  def main(args: Array[String]): Unit = {
+    val rng = SimpleRNG(42)
+    val (n1,rng2) = rng.nextInt
+    println(rng.nextInt)
+    println(rng.nonNegativeInt(rng))
+  }
+}
